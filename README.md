5c73f0aceb4320c8b18546892d40bd68fa6b5841
Joel Arvidsson <joel@oblador.se>
2015-05-15 19:06:13 +0200

    First implementation.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# react-native-font-icon
-Custom font icons for React Native
+# Vector Icons for React Native
+
+## Installation
+
+`$ npm install react-native-vector-icons --save`
+
+
+## Usage
+
+Generate your custom icon font at [fontello](http://fontello.com). 
+Add icon fonts to your project and remember to add it under "Fonts provided by application" in your `Info.plist`. 
+
+```
+var fontelloConfig = require('./config.json');
+var { createIconSetFromFontello } = require('react-native-vector-icons');
+var Icon = createIconSetFromFontello(fontelloConfig);
+
+<Icon name="facebook" size={30} style={{ padding: 5, color: '#fff', backgroundColor: '#3b5998' }} />
+´´´
---
+++ index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var _ = require('lodash');
+var React = require('react-native');
+var {
+  View,
+  Text,
+  StyleSheet,
+} = React;
+var merge = require('merge');
+var flattenStyle = require('flattenStyle');
+var ViewStylePropTypes = require('ViewStylePropTypes');
+var TextStylePropTypes = require('TextStylePropTypes');
+
+
+function createIconSet(glyphMap, fontFamily) {
+  var styles = StyleSheet.create({
+    container: {
+      overflow:         'hidden',
+      backgroundColor:  'transparent',
+      flexDirection:    'row',
+      justifyContent:   'flex-start',
+      alignItems:       'center',
+    },
+    text: {
+      fontFamily,
+    }
+  });
+
+  return React.createClass({
+    propTypes:{
+      name: React.PropTypes.oneOf(Object.keys(glyphMap)).isRequired,
+      size: React.PropTypes.number,
+      style: React.PropTypes.oneOfType([
+        React.PropTypes.number, // References to style sheets are numbers
+        React.PropTypes.object  // Inline style declaration
+      ])
+    },
+    render: function() {
+
+      var name = this.props.name;
+      var glyph = glyphMap[name] || '?';
+
+      var containerStyle = _.pick(flattenStyle([styles.container, this.props.style]), Object.keys(ViewStylePropTypes));
+
+      var textStyle = _.pick(
+        flattenStyle([this.props.style, styles.text]),
+        Object.keys(TextStylePropTypes)
+      );
+
+      var size = this.props.size || textStyle.fontSize || 12;
+      textStyle.fontSize    = size;
+      textStyle.lineHeight  = size;
+      textStyle.height      = size;
+
+      return (
+        <View {...this.props} style={[containerStyle]}>
+          <Text style={textStyle}>{glyph}</Text>
+          {this.props.children}
+        </View>
+      );
+    }
+  });
+};
+
+function createIconFromFontello(config, fontFamily) {
+  var glyphMap = {};
+  config.glyphs.forEach(function (glyph) {
+    glyphMap[glyph.css] = String.fromCharCode(glyph.code);
+  });
+  return createIconSet(glyphMap, fontFamily || config.name)
+};
+
+module.exports = {
+  createIconSet,
+  createIconSetFromFontello,
+};
