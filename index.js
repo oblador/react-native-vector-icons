16a2fb0f96568dc23301a480965e3a4ab3786dab
Jorge Maroto <patoroco@gmail.com>
2015-11-17 11:35:26 +0100

    Allow to load icons from icomoon templates

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 var createIconSet = require('./lib/create-icon-set');
 var createIconSetFromFontello = require('./lib/create-icon-set-from-fontello');
+var createIconSetFromIcoMoon = require('./lib/create-icon-set-from-icomoon');
 
 module.exports = {
   createIconSet,
   createIconSetFromFontello,
+  createIconSetFromIcoMoon
 };
