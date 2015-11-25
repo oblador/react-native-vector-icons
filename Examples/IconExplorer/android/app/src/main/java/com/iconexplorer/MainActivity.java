3f561ba46280f604af8848f6a99b8a426fb62cb5
Joel Arvidsson <joel@oblador.se>
2015-11-25 08:45:17 -0500

    Updates IconExplorer example to use RN 0.16-rc.

--- Examples/IconExplorer/android/app/src/main/java/com/iconexplorer/MainActivity.java
+++ Examples/IconExplorer/android/app/src/main/java/com/iconexplorer/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
         super.onResume();
 
         if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this);
+            mReactInstanceManager.onResume(this, this);
         }
     }
 }
