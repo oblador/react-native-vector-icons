72f3f2ca06c60790ceb02dce9224d2ee4470599d
Joel Arvidsson <joel@oblador.se>
2016-08-24 14:13:04 +0200

    Bumps examples to RN 0.32.

---
+++ Examples/IconExplorer/android/app/src/main/java/com/iconexplorer/MainApplication.java
@@ -0,0 +1,38 @@
+package com.iconexplorer;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.oblador.vectoricons.VectorIconsPackage;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new VectorIconsPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
