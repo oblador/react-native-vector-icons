ca62d31030580b3b58a91f0c7020c229800af151
Daniel Friesen <daniel@nadir-seen-fire.com>
2017-01-02 08:38:34 -0800

    Add icons from materialdesignicons.com as MaterialCommunityIcons.

---
+++ MaterialCommunityIcons.js
@@ -0,0 +1,18 @@
+/**
+ * MaterialCommunityIcons icon set component.
+ * Usage: <MaterialCommunityIcons name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from './lib/create-icon-set';
+import glyphMap from './glyphmaps/MaterialCommunityIcons.json';
+
+const iconSet = createIconSet(glyphMap, 'Material Design Icons', 'MaterialCommunityIcons.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
+
