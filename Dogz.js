3ff32b6a0a6a77574c52f4ae841043cabf846504
MikeDabrowski <mike2060@gmail.com>
2017-01-31 21:00:15 +0100

    Added new icon packs
    Added dogs' and cats' breeds icons

---
+++ Dogz.js
@@ -0,0 +1,18 @@
+/**
+ * Dogz icon set component.
+ * Usage: <Dogz name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from './lib/create-icon-set';
+import glyphMap from './glyphmaps/Dogz.json';
+
+const iconSet = createIconSet(glyphMap, 'Dogz', 'Dogz.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
+
