d9418da28f8d9dac62fc24f75458f1d4d466d3e6
Matthieu Lemoine <ml.94230@gmail.com>
2017-05-03 18:50:59 +0200

    Replace React.PropTypes with prop-types (#458)

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import Entypo from '../../glyphmaps/Entypo.json';
 import EvilIcons from '../../glyphmaps/EvilIcons.json';
