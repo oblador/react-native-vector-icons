c136adfead96fe917be5a98adf727d7846eaa775
Kyle Roach <kroach.work@gmail.com>
2017-06-10 02:50:28 -0400

    [Typings] Descriptions for IconProps

--- index.d.ts
+++ index.d.ts
@@ -5,20 +5,46 @@
 declare module 'react-native-vector-icons/*' {
   import * as React from 'react'
   import {
+    TextStyle,
     ViewStyle,
+    TextProperties,
     TouchableHighlightProperties,
     TouchableNativeFeedbackProperties
   } from 'react-native'
 
-  interface IconProps {
-    name: string
+  interface IconProps extends TextProperties {
+    /**
+     * Size of the icon, can also be passed as fontSize in the style object.
+     *
+     * @default 12
+     * @type {string}
+     * @memberof IconProps
+     */
     size: number
+
+    /**
+     * Name of the icon to show
+     *
+     * See Icon Explorer app
+     * {@link https://github.com/oblador/react-native-vector-icons/tree/master/Examples/IconExplorer}
+     * @type {string}
+     * @memberof IconProps
+     */
+    name: string
+
+    /**
+     * Color of the icon
+     *
+     * @type {string}
+     * @memberof IconProps
+     */
     color?: string
   }
 
   interface IconButtonProps extends IconProps, TouchableHighlightProperties, TouchableNativeFeedbackProperties {
     borderRadius?: number
     iconStyle?: ViewStyle
+    style?: ViewStyle | TextStyle
     backgroundColor?: string
   }
 
