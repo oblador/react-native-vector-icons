a29b705d83b89c44650837e129acc855d84b7182
Kyle Roach <kroach.work@gmail.com>
2017-06-10 02:32:18 -0400

    [Typings] Added basic typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for react-native-vector-icons
+// Project: https://github.com/oblador/react-native-vector-icons
+// Definitions by: Kyle Roach https://github.com/iRoachie
+
+declare module 'react-native-vector-icons/*' {
+  import * as React from 'react'
+  import {
+    ViewStyle,
+    TouchableHighlightProperties,
+    TouchableNativeFeedbackProperties
+  } from 'react-native'
+
+  interface IconProps {
+    name: string
+    size: number
+    color?: string
+  }
+
+  interface IconButtonProps extends IconProps, TouchableHighlightProperties, TouchableNativeFeedbackProperties {
+    borderRadius?: number
+    iconStyle?: ViewStyle
+    backgroundColor?: string
+    onPress(): void
+  }
+
+  class Icon extends React.Component<IconProps, any> { }
+
+  namespace Icon {
+    export class Icon {
+      static Button: React.Component<IconButtonProps, any>
+    }
+    export class Button extends React.Component<IconButtonProps, any> {}
+  }
+
+  export default Icon
+}
