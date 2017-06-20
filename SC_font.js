1125f9c2627cf403bf70c92a5f98017d67f069c2
Matej Rajcic <thesickbomber@gmail.com>
2017-06-21 00:37:13 +0200

    Added icons
    Necu pisatttttt

---
+++ SC_font.js
@@ -0,0 +1,18 @@
+/**
+ * FontAwesome icon set component.
+ * Usage: <FontAwesome name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from './lib/create-icon-set';
+import glyphMap from './glyphmaps/SC-font.json';
+
+const iconSet = createIconSet(glyphMap, 'SC-font', 'SC_font.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
+
