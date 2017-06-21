85aa2828695412b8b26f76ab211e14bb1b015b1a
Matej Rajcic <thesickbomber@gmail.com>
2017-06-21 23:46:19 +0200

    SC font update
    Sc font fix

--- SC_font.js
+++ SC_font.js
@@ -6,7 +6,7 @@
 import createIconSet from './lib/create-icon-set';
 import glyphMap from './glyphmaps/SC-font.json';
 
-const iconSet = createIconSet(glyphMap, 'SC-font', 'SC_font.ttf');
+const iconSet = createIconSet(glyphMap, 'SC-font', 'SC-font.ttf');
 
 export default iconSet;
 
