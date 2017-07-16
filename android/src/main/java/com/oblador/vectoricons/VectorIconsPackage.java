01942f15261d4fe50affc5b23b1273f4b4374eea
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2017-07-17 01:12:22 +1000

    Handle Android RN 0.47 breaking change

--- android/src/main/java/com/oblador/vectoricons/VectorIconsPackage.java
+++ android/src/main/java/com/oblador/vectoricons/VectorIconsPackage.java
@@ -23,7 +23,7 @@ public class VectorIconsPackage implements ReactPackage {
     return modules;
   }
 
-  @Override
+  // Deprecated RN 0.47
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
