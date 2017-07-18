f1622a0ccd44442ab3140a2369b42d05dd6438d7
fractalwrench <fractalwrench@gmail.com>
2017-07-18 15:29:42 +0100

    Remove override of createJSModules
    Fixes #515

--- android/src/main/java/com/oblador/vectoricons/VectorIconsPackage.java
+++ android/src/main/java/com/oblador/vectoricons/VectorIconsPackage.java
@@ -24,11 +24,6 @@ public class VectorIconsPackage implements ReactPackage {
   }
 
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
   }
