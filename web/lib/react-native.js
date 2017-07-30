937c17c74ef79efc2409542dfb1af50a5323b47b
Joel Arvidsson <joel@oblador.se>
2017-07-30 18:24:47 +0200

    Move built js files to dist folder and exclude from git

--- web/lib/react-native.js
+++
@@ -1,1 +0,0 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _reactNative=require('react-native');Object.keys(_reactNative).forEach(function(key){if(key==="default"||key==="__esModule")return;Object.defineProperty(exports,key,{enumerable:true,get:function get(){return _reactNative[key];}});});
