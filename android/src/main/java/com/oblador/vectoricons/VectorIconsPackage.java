1b8ba0b146ff663492c73ced28023d288dc92863
Joel Arvidsson <joel@oblador.se>
2017-07-30 13:41:20 +0200

    Diff nit

--- android/src/main/java/com/oblador/vectoricons/VectorIconsPackage.java
+++ android/src/main/java/com/oblador/vectoricons/VectorIconsPackage.java
@@ -15,10 +15,6 @@ public class VectorIconsPackage implements ReactPackage {
 
   public VectorIconsPackage() {}
 
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-  
   @Override
   public List<NativeModule> createNativeModules(
       ReactApplicationContext reactContext) {
@@ -27,6 +23,10 @@ public class VectorIconsPackage implements ReactPackage {
     return modules;
   }
 
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+  
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
