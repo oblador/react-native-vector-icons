53d845c0084b21fd25e510dfd7f070a7b6299b13
Ramon Kleiss <ramonkleiss@gmail.com>
2017-09-16 13:52:07 +0200

    Add support for Feather icons (#546)
    * Add support for Feather icons
    
    * Fix a small bug in the Feather.js icon set generation
    
    * Fix permissions of Feather.ttf
    
    * Fix the Feather font family and name
    
    * Add typings for the Feather module
    
    * Revert "Add support for Feather icons"
    
    This reverts commit 372006e766924ef50ee22b8d362c5762cf1db691.
    
    * Add dependency feather-icons
    
    * Add support for Feather icons
    
    * Add the Feather icons to the icon index
    
    * Update README to include Feather icons

---
+++ Feather.js
@@ -0,0 +1,18 @@
+/**
+ * Feather icon set component.
+ * Usage: <Feather name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from './lib/create-icon-set';
+import glyphMap from './glyphmaps/Feather.json';
+
+const iconSet = createIconSet(glyphMap, 'Feather', 'Feather.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
+
