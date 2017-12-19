f539633553af2e13aa4be46f97e18ddac505f1c4
Joel Arvidsson <joel@oblador.se>
2017-12-19 23:26:01 +0100

    Bump IconExplorer example to RN 0.51

--- Examples/IconExplorer/android/app/src/main/java/com/iconexplorer/MainApplication.java
+++ Examples/IconExplorer/android/app/src/main/java/com/iconexplorer/MainApplication.java
@@ -28,6 +28,11 @@ public class MainApplication extends Application implements ReactApplication {
           new VectorIconsPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index.android";
+    }
   };
 
   @Override
