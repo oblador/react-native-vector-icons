47cece260e7668159d90309f36e6f5a83ac57f62
Hampus Tågerud <hampus.tagerud@gmail.com>
2018-07-18 13:18:20 +0200

    Export all types in FontAwesome5 iconSets

--- FontAwesome5Pro.js
+++ FontAwesome5Pro.js
@@ -7,5 +7,14 @@ import { createFA5iconSet } from './lib/create-icon-set-from-fontawesome5';
 
 import glyphMap from './glyphmaps/FontAwesome5Pro.json';
 
+export { FA5Type } from './lib/create-icon-set-from-fontawesome5';
+
 const iconSet = createFA5iconSet(glyphMap, true);
+
 export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
