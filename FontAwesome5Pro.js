0f9b2a58055a1bdb5b4450511dec58b36b20aaf8
Hampus Tågerud <hampus.tagerud@gmail.com>
2018-07-18 11:26:45 +0200

    Create FontAwesome 5 iconSet with factory

--- FontAwesome5Pro.js
+++ FontAwesome5Pro.js
@@ -3,9 +3,9 @@
  * Usage: <FontAwesome5Pro name="icon-name" size={20} color="#4F8EF7" />
  */
 
-import {FA5iconSet} from './FontAwesome5';
+import { createFA5iconSet } from './lib/create-icon-set-from-fontawesome5';
 
 import glyphMap from './glyphmaps/FontAwesome5Pro.json';
 
-const iconSet = FA5iconSet(glyphMap, true);
+const iconSet = createFA5iconSet(glyphMap, true);
 export default iconSet;
