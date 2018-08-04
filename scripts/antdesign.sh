3854231da31cb59df664551b5c8ab8085deddd2c
Hampus Tågerud <hampus.tagerud@gmail.com>
2018-08-04 13:14:04 +0200

    Update build script to automate builds

--- scripts/antdesign.sh
+++ scripts/antdesign.sh
@@ -1,10 +1,20 @@
-#!/bin/bash -e
+#!/bin/sh
 
-node bin/generate-icon node_modules/@bodhiveggie/antd-icons/fonts/iconfont.css\
-  --prefix=.icon-\
-  --componentName=AntDesign\
-  --fontFamily=AntDesign\
-  --template=templates/separated-icon-set.tpl\
-  --glyphmap=glyphmaps/AntDesign.json\
-  > AntDesign.js
-cp node_modules/@bodhiveggie/antd-icons/fonts/iconfont.ttf Fonts/AntDesign.ttf
+TEMP=$(mktemp -d -t rnvi)
+pushd ${TEMP}
+curl -o font.zip -L https://github.com/ant-design/ant-design/releases/download/resource/iconfont-3.x.zip
+unzip -j -d font font.zip
+popd
+
+node bin/generate-icon ${TEMP}/font/iconfont.css\
+   --prefix=.icon-\
+   --componentName=AntDesign\
+   --fontFamily=AntDesign\
+   --template=templates/separated-icon-set.tpl\
+   --glyphmap=glyphmaps/AntDesign.json\
+   > AntDesign.js
+
+mv ${TEMP}/font/iconfont.ttf Fonts/AntDesign.ttf
+
+rm -r ${TEMP}
+
