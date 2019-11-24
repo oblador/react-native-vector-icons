9acd13e54e86db9ce43894e2839862303c528663
Joel Arvidsson <joel@oblador.se>
2019-11-24 13:37:05 +0100

    Generate font directory from source files

--- directory/src/index.js
+++ directory/src/index.js
@@ -1,8 +1,6 @@
-/* eslint-env browser */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
