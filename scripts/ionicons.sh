1ef4e1feec8a5bf03720374ed7675a4adf00396c
Vinh <thanh.vinh@hotmail.com>
2021-02-07 15:09:04 +0700

    Upgrade Ionicons to v5.4.0

--- scripts/ionicons.sh
+++ scripts/ionicons.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -e
 
-node bin/generate-icon node_modules/ionicons/dist/css/ionicons.css\
-  --prefix=.ion-\
+curl https://raw.githubusercontent.com/ez-connect/flutter-ionicons/master/assets/ionicons.css > $TEMP_DIR/ionicons.css
+
+node bin/generate-icon $TEMP_DIR/ionicons.css\
+  --prefix=.icon-\
   --componentName=Ionicons\
   --fontFamily=Ionicons\
   --template=templates/separated-icon-set.tpl\
   --glyphmap=glyphmaps/Ionicons.json\
   > Ionicons.js
-cp node_modules/ionicons/dist/fonts/ionicons.ttf Fonts/Ionicons.ttf
+# cp node_modules/ionicons/dist/fonts/ionicons.ttf Fonts/Ionicons.ttf
