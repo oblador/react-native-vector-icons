7ee193fa528be99304a1f2e8ce712950f2937899
Joel Arvidsson <joel@oblador.se>
2023-07-17 12:03:28 +0200

    Upgrade example to React Native 0.72

--- Examples/IconExplorer/__tests__/App.test.tsx
+++ Examples/IconExplorer/__tests__/App.test.tsx
@@ -4,7 +4,10 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../src/App';
+import App from '../App';
+
+// Note: import explicitly to use the types shiped with jest.
+import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
