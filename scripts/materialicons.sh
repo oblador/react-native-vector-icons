cfdc6f0fa55ca49bfecc20c14f919bcc1d499f6d
John Ferlito <johnf@inodes.org>
2023-11-07 22:10:28 +1100

    Fix materialicons script

--- scripts/materialicons.sh
+++ scripts/materialicons.sh
@@ -4,7 +4,7 @@
 # artifacts are >500MB unpacked. Until that is fixed we'll download
 # them directly from the git repo.
 
-TEMP_DIR=`mktemp -d -t rnvi`
+TEMP_DIR=`mktemp -d -t rnvi.XXX`
 REF="f7bd4f25f3764883717c09a1fd867f560c9a9581" # Sep 19, 2022 update
 
 curl https://raw.githubusercontent.com/google/material-design-icons/$REF/font/MaterialIcons-Regular.ttf -Ls > Fonts/MaterialIcons.ttf
