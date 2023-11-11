fb12ac6f7e2ef65bc0a4ed6952544c26bbf40b84
John Ferlito <johnf@inodes.org>
2023-11-12 07:19:36 +1100

    WIP: ts

---
+++ packages/common/src/NativeVectorIcons.ts
@@ -0,0 +1,22 @@
+import type { TurboModule } from 'react-native';
+import { TurboModuleRegistry } from 'react-native';
+
+export interface Spec extends TurboModule {
+  getImageForFont(
+    fontName: string,
+    glyph: string,
+    fontSize: number,
+    color: number
+  ): Promise<string>;
+
+  getImageForFontSync(
+    fontName: string,
+    glyph: string,
+    fontSize: number,
+    color: number
+  ): string;
+
+  loadFontWithFileName(fontFileName: string, extension: string): Promise<void>;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>('VectorIcons');
