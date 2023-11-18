9a079eb6d2bac2f29e90e9d4ad1c338eaefc12f3
John Ferlito <johnf@inodes.org>
2023-11-18 13:44:04 +1100

    update script for ant design and fontawesome scripts

--- packages/antdesign/scripts/update.sh
+++ packages/antdesign/scripts/update.sh
@@ -5,16 +5,17 @@ set -e
 TEMP=$(mktemp -q -d -t rnvi.XXX)
 
 pushd ${TEMP}
-curl -s -o font.zip -L https://github.com/ant-design/ant-design/releases/download/resource/iconfont-3.x.zip
+# curl -s -o font.zip -L https://github.com/ant-design/ant-design/releases/download/resource/iconfont-3.x.zip
+curl -s -o font.zip -L file:///home/johnf/Downloads/iconfont-3.x.zip
 unzip -q -j -d font font.zip
 popd
 
-node bin/generate-icon ${TEMP}/font/iconfont.css\
+generate-icon ${TEMP}/font/iconfont.css\
    --prefix=.icon-\
    --componentName=AntDesign\
    --fontFamily=anticon\
-   --template=templates/separated-icon-set.tpl\
+   --template=../common/templates/separated-icon-set.tpl\
    --glyphmap=glyphmaps/AntDesign.json\
-   > AntDesign.js
+   > index.tsx
 
-mv ${TEMP}/font/iconfont.ttf Fonts/AntDesign.ttf
+mv ${TEMP}/font/iconfont.ttf fonts/AntDesign.ttf
