0d23a1bb4fa255126a32903ea5e73388d074937e
John Ferlito <johnf@inodes.org>
2023-11-18 14:13:51 +1100

    WIP: set up evilicons and ftoncustom script

---
+++ packages/common/src/bin/fontcustom.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { execSync } from 'node:child_process';
+
+const cmd = `echo docker run --rm --interactive --tty \
+   --volume $(pwd):/app/project \
+   --user $(id -u):$(id -g) \
+   telor/fontcustom-worker \
+   fontcustom ${process.argv.slice(2).join(' ')}
+`;
+
+const stdout = execSync(cmd);
+console.log(stdout.toString());
--- packages/evilicons/scripts/update.sh
+++ packages/evilicons/scripts/update.sh
@@ -1,20 +1,25 @@
 #!/bin/bash -e
 
-TEMP_DIR=tmp
-./scripts/fontcustom compile node_modules/evil-icons/assets/icons \
+set -e
+
+TEMP_DIR=$(mktemp -q -d -t rnvi.XXX)
+
+fontcustom compile node_modules/evil-icons/assets/icons \
   --output $TEMP_DIR \
   --name EvilIcons \
   --templates css \
   --force \
   --no-hash
 
-node bin/generate-icon.js $TEMP_DIR/EvilIcons.css \
+generate-icon $TEMP_DIR/EvilIcons.css \
   --prefix=.icon-ei- \
   --componentName=EvilIcons \
-  --template=templates/separated-icon-set.tpl \
-  --glyphmap=glyphmaps/EvilIcons.json \
   --fontFamily=EvilIcons \
-  > EvilIcons.js
-cp $TEMP_DIR/EvilIcons.ttf Fonts
+  --template=../common/templates/separated-icon-set.tpl \
+  --glyphmap=glyphmaps/EvilIcons.json \
+  > index.tsx
+
+cp $TEMP_DIR/EvilIcons.ttf fonts
+
 rm -rf $TEMP_DIR
 rm -rf .fontcustom-manifest.json
