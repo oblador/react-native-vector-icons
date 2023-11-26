213484b55d71f3b38d7b3643d89b21e99ae4f7ac
John Ferlito <johnf@inodes.org>
2023-11-26 16:10:30 +1100

    COnvert simple line icons

---
+++ packages/simplelineicons/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * SimpleLineIcons icon set component.
+ * Usage: <SimpleLineIcons name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import { createIconSet } from '@react-native-vector-icons/common';
+import glyphMap from '../glyphmaps/SimpleLineIcons.json';
+
+const Icon = createIconSet(glyphMap, 'simple-line-icons', 'SimpleLineIcons.ttf');
+
+Icon.loadFont();
+
+export default Icon;
+export const { Button, getImageSource, getImageSourceSync } = Icon;
+
