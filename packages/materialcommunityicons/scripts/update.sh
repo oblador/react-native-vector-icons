fd1d06f149953e20cf2776d223023205554aec06
John Ferlito <johnf@inodes.org>
2023-11-26 14:59:35 +1100

    convert mateiralcommunityicons

--- packages/materialcommunityicons/scripts/update.sh
+++ packages/materialcommunityicons/scripts/update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
-node bin/generate-icon node_modules/@mdi/font/css/materialdesignicons.css\
-  --prefix=.mdi-\
-  --componentName=MaterialCommunityIcons\
-  --fontFamily='Material Design Icons'\
-  --template=templates/separated-icon-set.tpl\
-  --glyphmap=glyphmaps/MaterialCommunityIcons.json\
-  > MaterialCommunityIcons.js
-cp node_modules/@mdi/font/fonts/materialdesignicons-webfont.ttf Fonts/MaterialCommunityIcons.ttf
+generate-icon ../../node_modules/@mdi/font/css/materialdesignicons.css \
+  --prefix .mdi-\
+  --componentName MaterialCommunityIcons\
+  --fontFamily 'Material Design Icons'\
+  --template ../common/templates/separated-icon-set.tpl \
+  --glyphmap glyphmaps/MaterialCommunityIcons.json\
+  > src/index.ts
+
+cp ../../node_modules/@mdi/font/fonts/materialdesignicons-webfont.ttf fonts/MaterialCommunityIcons.ttf
