c19e7e9ac13eb6f851a66f27b955a9d7e5664584
John Ferlito <johnf@inodes.org>
2023-11-26 12:22:01 +1100

    Update antdesign

--- packages/antdesign/scripts/update.sh
+++ packages/antdesign/scripts/update.sh
@@ -1,12 +1,11 @@
-#!/bin/env bash
+#!/bin/bash
 
 set -e
 
 TEMP_DIR=$(mktemp -q -d -t rnvi.XXX)
 
 pushd ${TEMP_DIR}
-# curl -s -o font.zip -L https://github.com/ant-design/ant-design/releases/download/resource/iconfont-3.x.zip
-curl -s -o font.zip -L file:///home/johnf/Downloads/iconfont-3.x.zip
+curl -s -o font.zip -L https://github.com/ant-design/ant-design/releases/download/resource/iconfont-3.x.zip
 unzip -q -j -d font font.zip
 popd
 
@@ -16,7 +15,7 @@ generate-icon ${TEMP_DIR}/font/iconfont.css \
   --fontFamily=anticon \
   --template=../common/templates/separated-icon-set.tpl \
   --glyphmap=glyphmaps/AntDesign.json \
-  > index.tsx
+  > src/index.tsx
 
 mv ${TEMP_DIR}/font/iconfont.ttf fonts/AntDesign.ttf
 
