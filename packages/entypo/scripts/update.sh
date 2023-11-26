9677ade5cec4d3f0aac5a2a8a59de1103152c637
John Ferlito <johnf@inodes.org>
2023-11-26 12:16:50 +1100

    update entypo

--- packages/entypo/scripts/update.sh
+++ packages/entypo/scripts/update.sh
@@ -1,20 +1,24 @@
-#!/bin/bash -e
+#!/bin/bash
 
-TEMP_DIR=tmp
+set -e
 
-./scripts/fontcustom compile node_modules/@entypo-icons/core/icons \
+TEMP_DIR=$(mktemp -q -d -t rnvi.XXX -p .)
+
+fontcustom compile ../node_modules/@entypo-icons/core/icons \
   --output $TEMP_DIR \
   --name Entypo \
   --templates css \
   --force \
   --no-hash
 
-node bin/generate-icon.js $TEMP_DIR/Entypo.css\
-  --componentName=Entypo \
-  --fontFamily=Entypo \
-  --template=templates/separated-icon-set.tpl \
-  --glyphmap=glyphmaps/Entypo.json \
-  > Entypo.js
-cp $TEMP_DIR/Entypo.ttf Fonts
+generate-icon $TEMP_DIR/Entypo.css \
+  --componentName Entypo \
+  --fontFamily Entypo \
+  --template ../common/templates/separated-icon-set.tpl \
+  --glyphmap glyphmaps/Entypo.json \
+  > src/index.tsx
+
+cp $TEMP_DIR/Entypo.ttf fonts
+
 rm -rf $TEMP_DIR
 rm -rf .fontcustom-manifest.json
