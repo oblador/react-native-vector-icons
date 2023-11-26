a5dd50bbe2fab34ab9a51048be9bfef86993ba8e
John Ferlito <johnf@inodes.org>
2023-11-26 12:26:57 +1100

    Update evilicons

--- packages/antdesign/scripts/update.sh
+++ packages/antdesign/scripts/update.sh
@@ -10,11 +10,11 @@ unzip -q -j -d font font.zip
 popd
 
 generate-icon ${TEMP_DIR}/font/iconfont.css \
-  --prefix=.icon- \
-  --componentName=AntDesign \
-  --fontFamily=anticon \
-  --template=../common/templates/separated-icon-set.tpl \
-  --glyphmap=glyphmaps/AntDesign.json \
+  --prefix .icon- \
+  --componentName AntDesign \
+  --fontFamily anticon \
+  --template ../common/templates/separated-icon-set.tpl \
+  --glyphmap glyphmaps/AntDesign.json \
   > src/index.tsx
 
 mv ${TEMP_DIR}/font/iconfont.ttf fonts/AntDesign.ttf
