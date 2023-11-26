128a3afea73cbb0f9277a5799acfc66debd2ca6e
John Ferlito <johnf@inodes.org>
2023-11-26 16:10:30 +1100

    COnvert simple line icons

---
+++ packages/simplelineicons/android/src/main/java/com/oblador/vectoricons/simplelineicons/SimpleLineIconsVectorIconsPackage.java
@@ -0,0 +1,24 @@
+package com.oblador.vectoricons.simplelineicons;
+
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.module.model.ReactModuleInfo;
+import com.facebook.react.module.model.ReactModuleInfoProvider;
+import com.facebook.react.TurboReactPackage;
+
+import java.util.HashMap;
+
+public class SImpleLineIconsVectorIconsPackage extends TurboReactPackage {
+  @Nullable
+  @Override
+  public NativeModule getModule(String name, ReactApplicationContext reactContext) {
+      return null;
+  }
+
+  @Override
+  public ReactModuleInfoProvider getReactModuleInfoProvider() {
+    return () -> new HashMap<>();
+  }
+}
