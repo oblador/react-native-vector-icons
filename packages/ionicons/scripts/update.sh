ff2b9c52eb1f7db2dbfe27a2e3dab42fc99d1637
John Ferlito <johnf@inodes.org>
2023-11-26 14:46:29 +1100

    convert ionicons

--- packages/ionicons/scripts/update.sh
+++ packages/ionicons/scripts/update.sh
@@ -1,7 +1,28 @@
 #!/bin/bash -e
 
-mkdir -p Ioniconstmp
-node node_modules/oslllo-svg-fixer/src/cli.js -s node_modules/ionicons/dist/svg -d Ioniconstmp
-node bin/generate-ionicons.js
-rm -rf Ioniconstmp
-rm -rf Ionicons.svg
+set -e
+
+TEMP_DIR=$(mktemp -q -d -t rnvi.XXX -p .)
+mkdir $TEMP_DIR/svg
+
+oslllo-svg-fixer -s ../../node_modules/ionicons/dist/svg -d $TEMP_DIR/svg
+
+fontcustom compile $TEMP_DIR/svg \
+  --output $TEMP_DIR \
+  --name Ionicons \
+  --templates css \
+  --force \
+  --no-hash
+
+  # --prefix .icon-ei- \
+generate-icon $TEMP_DIR/Ionicons.css \
+  --componentName Ionicons \
+  --fontFamily Ionicons \
+  --template ../common/templates/separated-icon-set.tpl \
+  --glyphmap glyphmaps/Ionicons.json \
+  > src/index.tsx
+
+cp $TEMP_DIR/Ionicons.ttf fonts
+
+rm -rf $TEMP_DIR
+rm -rf .fontcustom-manifest.json
---
+++ packages/ionicons/src/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * Ionicons icon set component.
+ * Usage: <Ionicons name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import { createIconSet } from '@react-native-vector-icons/common';
+import glyphMap from '../glyphmaps/Ionicons.json';
+
+const Icon = createIconSet(glyphMap, 'Ionicons', 'Ionicons.ttf');
+
+Icon.loadFont();
+
+export default Icon;
+export const { Button, getImageSource, getImageSourceSync } = Icon;
+
