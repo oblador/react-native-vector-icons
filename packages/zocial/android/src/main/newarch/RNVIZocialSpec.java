8c30df2375c281b0f9c49108b3c3a66439626fea
John Ferlito <johnf@inodes.org>
2023-12-02 16:14:06 +1100

    auto generate simple-line-icons

---
+++ packages/zocial/android/src/main/newarch/RNVIZocialSpec.java
@@ -0,0 +1,9 @@
+package com.reactnativevectoricons.zocial;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class RNVIZocialSpec extends NativeRNVIZocialSpec {
+  RNVIZocialSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
---
+++ packages/zocial/android/src/main/oldarch/RNVIZocialSpec.java
@@ -0,0 +1,11 @@
+package com.reactnativevectoricons.zocial;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.Promise;
+
+abstract class RNVIZocialSpec extends ReactContextBaseJavaModule {
+  RNVIZocialSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
