e5e5552e57a3e5efad69df599193d0827e26f152
John Ferlito <johnf@inodes.org>
2023-12-02 16:11:36 +1100

    auto generate simple-line-icons

---
+++ packages/simple-line-icons/android/src/main/newarch/RNVISimpleLineIconsSpec.java
@@ -0,0 +1,9 @@
+package com.reactnativevectoricons.simple_line_icons;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class RNVISimpleLineIconsSpec extends NativeRNVISimpleLineIconsSpec {
+  RNVISimpleLineIconsSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
