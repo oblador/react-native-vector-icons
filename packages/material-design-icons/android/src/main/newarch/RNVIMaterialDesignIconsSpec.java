54604b5192fd052619354248176fd29a75c7d535
John Ferlito <johnf@inodes.org>
2023-12-02 15:08:17 +1100

    auto generate material-design-icons

---
+++ packages/material-design-icons/android/src/main/newarch/RNVIMaterialDesignIconsSpec.java
@@ -0,0 +1,9 @@
+package com.reactnativevectoricons.material_design_icons;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class RNVIMaterialDesignIconsSpec extends NativeRNVIMaterialDesignIconsSpec {
+  RNVIMaterialDesignIconsSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
