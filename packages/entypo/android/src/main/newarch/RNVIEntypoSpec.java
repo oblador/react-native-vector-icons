5a3ded6ded3926fa32465e615a605ad7f34ca61d
John Ferlito <johnf@inodes.org>
2023-12-02 12:54:07 +1100

    Generator working for entypo

---
+++ packages/entypo/android/src/main/newarch/RNVIEntypoSpec.java
@@ -0,0 +1,9 @@
+package com.reactnativevectoricons.entypo;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class RNVIEntypoSpec extends NativeRNVIEntypoSpec {
+  RNVIEntypoSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
