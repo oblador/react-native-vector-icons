acaa9d09f47fba403bd40469a5a34283843aa4b0
John Ferlito <johnf@inodes.org>
2023-12-02 14:34:13 +1100

    auto generate ionicons

---
+++ packages/ionicons/android/src/main/oldarch/RNVIIoniconsSpec.java
@@ -0,0 +1,11 @@
+package com.reactnativevectoricons.ionicons;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.Promise;
+
+abstract class RNVIIoniconsSpec extends ReactContextBaseJavaModule {
+  RNVIIoniconsSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
