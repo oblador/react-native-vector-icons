881d5cdb8c7371b3b724ebb8406fab9a3c402714
John Ferlito <johnf@inodes.org>
2023-12-02 14:16:42 +1100

    auto generate fontawesome

---
+++ packages/fontawesome/android/src/main/newarch/RNVIFontawesomeSpec.java
@@ -0,0 +1,9 @@
+package com.reactnativevectoricons.fontawesome;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class RNVIFontawesomeSpec extends NativeRNVIFontawesomeSpec {
+  RNVIFontawesomeSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
