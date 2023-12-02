435239c6d967baa1ef5545c274a52f215a0d4060
John Ferlito <johnf@inodes.org>
2023-12-02 14:23:07 +1100

    auto generate fontisto

---
+++ packages/fontisto/android/src/main/java/com/oblador/vectoricons/fontisto/RNVIFontistoPackage.java
@@ -0,0 +1,45 @@
+package com.oblador.vectoricons.fontisto;
+
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.module.model.ReactModuleInfo;
+import com.facebook.react.module.model.ReactModuleInfoProvider;
+import com.facebook.react.TurboReactPackage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RNVIFontistoPackage extends TurboReactPackage {
+
+  @Nullable
+  @Override
+  public NativeModule getModule(String name, ReactApplicationContext reactContext) {
+    if (name.equals(RNVIFontistoModule.NAME)) {
+      return new RNVIFontistoModule(reactContext);
+    } else {
+      return null;
+    }
+  }
+
+  @Override
+  public ReactModuleInfoProvider getReactModuleInfoProvider() {
+    return () -> {
+      final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+      boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+      moduleInfos.put(
+              AntDesignModule.NAME,
+              new ReactModuleInfo(
+                      RNVIFontistoModule.NAME,
+                      RNVIFontistoAntDesignModule.NAME,
+                      false, // canOverrideExistingModule
+                      false, // needsEagerInit
+                      true, // hasConstants
+                      false, // isCxxModule
+                      isTurboModule // isTurboModule
+      ));
+      return moduleInfos;
+    };
+  }
+}
