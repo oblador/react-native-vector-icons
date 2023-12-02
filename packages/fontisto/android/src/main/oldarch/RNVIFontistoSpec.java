9b8716543a4022973186de567508610ded438421
John Ferlito <johnf@inodes.org>
2023-12-02 14:23:07 +1100

    auto generate fontisto

---
+++ packages/fontisto/android/src/main/oldarch/RNVIFontistoSpec.java
@@ -0,0 +1,11 @@
+package com.reactnativevectoricons.fontisto;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.Promise;
+
+abstract class RNVIFontistoSpec extends ReactContextBaseJavaModule {
+  RNVIFontistoSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
