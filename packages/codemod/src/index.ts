5ac199dcc204428b6f2f22bd066b3bf3b4fe8ee0
John Ferlito <johnf@inodes.org>
2023-12-29 03:32:42 +1100

    codemod updates and update README and MIGRATION

--- packages/codemod/src/index.ts
+++ packages/codemod/src/index.ts
@@ -1,10 +1,43 @@
 #!/uar/bin/env node
 
 import path from 'node:path';
-import { execSync } from 'node:child_process';
+import { exec } from 'node:child_process';
+import packageJsonTransform from './package-json';
+import infoPlistTransform from './info-plist';
+import removeFonts from './remove-fonts';
 
 const dir = process.argv[2] || '.';
 
-const transformFilePath = path.join(__dirname, 'import-transform.js');
+const transformFilePath = path.join(__dirname, 'transform.js');
 const cmd = `jscodeshift --extensions=js,ts,jsx,tsx --parser tsx -t ${transformFilePath} ${dir}`;
-execSync(cmd, { stdio: 'inherit' });
+
+const proc = exec(cmd, { env: { ...process.env, FORCE_COLOR: 'true' } });
+
+const pkgs = new Set<string>();
+proc.stdout?.on('data', (data: string) => {
+  console.log(data);
+
+  const lines = data.split('\n');
+
+  lines.forEach((line) => {
+    if (line.match('DEP_FOUND: ')) {
+      pkgs.add(line.replace(/.*DEP_FOUND: /, '').trim());
+    }
+  });
+});
+
+proc.stderr?.on('data', (data) => {
+  console.error(data);
+});
+
+proc.on('exit', () => {
+  packageJsonTransform(pkgs);
+  infoPlistTransform();
+  removeFonts();
+
+  console.log(`
+Transform complete! You may need to run 'yarn install' or 'npm install' to install new dependencies.
+
+Please check https://github.com/react-native-vector-icons/react-native-vector-icons/blob/master/MIGRATION.md for any manual steps
+  `);
+});
---
+++ packages/codemod/src/remove-fonts.ts
@@ -0,0 +1,49 @@
+import fs from 'node:fs';
+import { execSync } from 'node:child_process';
+
+const fonts = [
+  'AntDesign.ttf',
+  'Entypo.ttf',
+  'EvilIcons.ttf',
+  'Feather.ttf',
+  'FontAwesome5_Brands.ttf',
+  'FontAwesome5_Regular.ttf',
+  'FontAwesome5_Solid.ttf',
+  'FontAwesome6_Brands.ttf',
+  'FontAwesome6_Regular.ttf',
+  'FontAwesome6_Solid.ttf',
+  'FontAwesome.ttf',
+  'Fontisto.ttf',
+  'Foundation.ttf',
+  'Ionicons.ttf',
+  'MaterialCommunityIcons.ttf',
+  'MaterialIcons.ttf',
+  'Octicons.ttf',
+  'SimpleLineIcons.ttf',
+  'Zocial.ttf',
+  'FontAwesome5_Pro_Light.ttf',
+  'FontAwesome5_Pro_Brands.ttf',
+  'FontAwesome5_Pro_Regular.ttf',
+  'FontAwesome5_Pro_Solid.ttf"',
+  'FontAwesome6_Pro_Light.ttf',
+  'FontAwesome6_Pro_Brands.ttf',
+  'FontAwesome6_Pro_Regular.ttf',
+  'FontAwesome6_Pro_Solid.ttf',
+  'FontAwesome6_Pro_Duotone.ttf',
+  'FontAwesome6_Pro_Thin.ttf',
+  'FontAwesome6_Pro_Sharp_Solid.ttf',
+  'FontAwesome6_Pro_Sharp_Light.ttf',
+  'FontAwesome6_Pro_Sharp_Regular.ttf',
+];
+
+export default () => {
+  console.log('Removing unused fonts');
+  const files = execSync('find android/app/src/main/assets/fonts -name "*.ttf"').toString().split("\n").map((line)=> line.trim());
+
+  const toDelete = files.filter((file) => fonts.includes(file.replace(/.*\//, '')));
+
+  toDelete.forEach((file) => {
+    console.log(` - Removing ${file}`);
+    fs.rmSync(file);
+  });
+};
