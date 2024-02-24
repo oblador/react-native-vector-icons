599b473d17c4b7feb2598eb4ee6309f80f4e8065
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-02-24 00:25:59 -0300

    Update material icons script to support variants

--- scripts/materialicons.sh
+++ scripts/materialicons.sh
@@ -6,16 +6,36 @@
 
 TEMP_DIR=`mktemp -d -t rnvi`
 REF="f7bd4f25f3764883717c09a1fd867f560c9a9581" # Sep 19, 2022 update
+BASE_MATERIAL_ICONS_URL="https://raw.githubusercontent.com/google/material-design-icons/$REF/font"
 
-curl https://raw.githubusercontent.com/google/material-design-icons/$REF/font/MaterialIcons-Regular.ttf -Ls > Fonts/MaterialIcons.ttf
+echo "Downloading MaterialIcons fonts"
+curl $BASE_MATERIAL_ICONS_URL/MaterialIcons-Regular.ttf -Ls > Fonts/MaterialIcons_Regular.ttf
+curl $BASE_MATERIAL_ICONS_URL/MaterialIcons-Regular.codepoints -Ls > $TEMP_DIR/MaterialIcons_Regular.codepoints
 
-curl https://raw.githubusercontent.com/google/material-design-icons/$REF/font/MaterialIcons-Regular.codepoints -Ls > $TEMP_DIR/MaterialIcons-Regular.codepoints
+curl $BASE_MATERIAL_ICONS_URL/MaterialIconsOutlined-Regular.otf -Ls > Fonts/MaterialIcons_Outlined.otf
+curl $BASE_MATERIAL_ICONS_URL/MaterialIconsOutlined-Regular.codepoints -Ls > $TEMP_DIR/MaterialIcons_Outlined.codepoints
 
-node bin/generate-material-icons $TEMP_DIR/MaterialIcons-Regular.codepoints\
-  --componentName=MaterialIcons\
-  --fontFamily='Material Icons'\
-  --template=templates/separated-icon-set.tpl\
-  --glyphmap=glyphmaps/MaterialIcons.json\
-  > MaterialIcons.js
+curl $BASE_MATERIAL_ICONS_URL/MaterialIconsRound-Regular.otf -Ls > Fonts/MaterialIcons_Round.otf
+curl $BASE_MATERIAL_ICONS_URL/MaterialIconsRound-Regular.codepoints -Ls > $TEMP_DIR/MaterialIcons_Round.codepoints
+
+curl $BASE_MATERIAL_ICONS_URL/MaterialIconsSharp-Regular.otf -Ls > Fonts/MaterialIcons_Sharp.otf
+curl $BASE_MATERIAL_ICONS_URL/MaterialIconsSharp-Regular.codepoints -Ls > $TEMP_DIR/MaterialIcons_Sharp.codepoints
+
+echo "Creating glyphmaps"
+node bin/generate-material-icons $TEMP_DIR/MaterialIcons_Regular.codepoints \
+  --addCodepoints=$TEMP_DIR/MaterialIcons_Outlined.codepoints \
+  --addCodepoints=$TEMP_DIR/MaterialIcons_Round.codepoints \
+  --addCodepoints=$TEMP_DIR/MaterialIcons_Sharp.codepoints \
+  --componentName=MaterialIcons \
+  --fontFamily='Material Icons' \
+  --glyphmap=glyphmaps/MaterialIcons.json > /dev/null
+
+echo "Generating metadata"
+node bin/generate-material-icons-metadata \
+  --path $TEMP_DIR/MaterialIcons_Regular.codepoints \
+    $TEMP_DIR/MaterialIcons_Outlined.codepoints \
+    $TEMP_DIR/MaterialIcons_Round.codepoints \
+    $TEMP_DIR/MaterialIcons_Sharp.codepoints \
+  --output glyphmaps/MaterialIcons_meta.json
 
 rm -rf $TEMP_DIR
