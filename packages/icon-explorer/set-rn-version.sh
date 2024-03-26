5bb7150afa5c1e9ffe403c56090fea0f43f2e4f2
John Ferlito <johnf@inodes.org>
2024-03-26 20:43:47 +1100

    Add rn version setter

---
+++ packages/icon-explorer/set-rn-version.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+set -e
+
+TAG=$1
+
+if [ -z "$TAG" ]; then
+  echo "Please provide a valid RN tag"
+  exit 1
+fi
+
+case "$TAG" in
+  latest)
+    VERSION=0.73
+    ;;
+  *-stable)
+    VERSION=${TAG//-stable/}
+    ;;
+  *)
+    echo "Invalid RN tag: $TAG"
+    exit 1
+    ;;
+esac
+
+rm -rf android/app/build/ android/.gradle/
+killall java 2>/dev/null || true
+
+if [ "$TAG" = "latest" ]; then
+  echo "No need to switch we are always set up for latest"
+  exit 0
+fi
+
+echo "Switching to $VERSION"
+
+yarn add "react-native@$TAG"
+
+if [ "$VERSION" = "0.70" ]; then
+  GRADLE_VERSION=7.5.1
+  DETOX_LEGACY=true
+  LEGACY_BABEL=true
+fi
+
+if [ "$VERSION" = "0.71" ]; then
+  GRADLE_VERSION=7.5.1
+  DETOX_LEGACY=true
+  LEGACY_BABEL=true
+fi
+
+if [ "$VERSION" = "0.72" ]; then
+  GRADLE_VERSION=8.0.1
+  DETOX_LEGACY=true
+  LEGACY_BABEL=true
+fi
+
+if [ -n "$GRADLE_VERSION" ]; then
+  echo "Setting gradle version to $GRADLE_VERSION"
+  sed -i "s/8.6/$GRADLE_VERSION/" android/gradle/wrapper/gradle-wrapper.properties
+fi
+
+if [ "$DETOX_LEGACY" = "true" ]; then
+  echo "Switch to detox-legacy"
+  sed -i 's/:detox:/:detox-legacy:/' android/build.gradle
+fi
+
+if [ "$LEGACY_BABEL" = "true" ]; then
+  echo "Switch to legacy babel"
+  sed -i 's/module:@react-native\/babel-preset/module:metro-react-native-babel-preset/' babel.config.js
+fi
+
+yarn rnx-align-deps --write
