d186fa0e0ad62ff1e9c4a95b50cae1e5dec5c714
John Ferlito <johnf@inodes.org>
2024-03-30 16:48:34 +1100

    Set iphone version

--- packages/icon-explorer/.detoxrc.js
+++ packages/icon-explorer/.detoxrc.js
@@ -38,7 +38,7 @@ module.exports = {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 15',
+        type: 'iPhone 14 Pro Max',
       },
     },
     attached: {
