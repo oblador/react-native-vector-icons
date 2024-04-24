21d8825dfff125061502f23d46975ab0a2f6cef4
John Ferlito <johnf@inodes.org>
2024-04-24 18:20:10 +1000

    codemod: add the actual version number to package.json

--- packages/codemod/src/package-json.ts
+++ packages/codemod/src/package-json.ts
@@ -1,18 +1,22 @@
 import fs from 'node:fs';
 
-export default (pkgs: Set<string>) => {
+const getVersion = async (pkg: string) => {
+  const packageJson = await fetch(`https://registry.npmjs.org/${pkg}/latest`).then((res) => res.json());
+  return `^${packageJson.version}`;
+}
+export default async (pkgs: Set<string>) => {
   const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
   const { dependencies } = packageJson;
 
-  pkgs.forEach((pkg) => {
+  pkgs.forEach(async (pkg) => {
     if (!dependencies[pkg]) {
-      dependencies[pkg] = 'latest';
+      dependencies[pkg] = await getVersion(pkg);
     }
   });
 
   if (pkgs.size > 0 && dependencies['react-native-vector-icons']) {
     dependencies['react-native-vector-icons'] = undefined;
-    dependencies['@react-native-vector-icons/common'] = 'latest';
+    dependencies['@react-native-vector-icons/common'] = await getVersion('@react-native-vector-icons/common');
     fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
   }
 };
