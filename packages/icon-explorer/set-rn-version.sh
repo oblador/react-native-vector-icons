b47d7f1ac0c358c4246b41ab5066e764c7465a23
John Ferlito <johnf@inodes.org>
2024-09-29 13:46:59 +1000

    fix: make owl work on ios

--- packages/icon-explorer/set-rn-version.sh
+++ packages/icon-explorer/set-rn-version.sh
@@ -47,7 +47,8 @@ case $VERSION in
 esac
 
 echo "Setting gradle version to $GRADLE_VERSION"
-sed -i'' -e "s/gradle-[0-9.]*-bin.zip/gradle-$GRADLE_VERSION-bin.zip/" android/gradle/wrapper/gradle-wrapper.properties
+sed -i.bak "s/gradle-[0-9.]*-bin.zip/gradle-$GRADLE_VERSION-bin.zip/" android/gradle/wrapper/gradle-wrapper.properties
+rm android/gradle/wrapper/gradle-wrapper.properties.bak
 
 yarn rnx-align-deps --requirements react-native@"$VERSION" --write
 LATEST_VERSION="^$(npm info react-native@^0.75 version --json 2>/dev/null | jq -r '.[-1]')"
@@ -57,9 +58,10 @@ yarn add react-native@"$LATEST_VERSION" @react-native/babel-preset@"$LATEST_VERS
 yarn add react-native-test-app@latest
 
 ## test-app doesn't bundle but we need it for react-native-owl
-sed -i'' \
+sed -i.bak \
   -e 's/task.enabled = false/task.enabled = true/;s/bundleInRelease      : false/bundleInRelease      : true/' \
   ../../node_modules/react-native-test-app/android/app/build.gradle
+rm ../../node_modules/react-native-test-app/android/app/build.gradle.bak
 
 yarn --no-immutable
 
