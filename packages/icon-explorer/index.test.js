0d6aee166856afc02a0f2b6c8af8a106d867221a
John Ferlito <johnf@inodes.org>
2024-10-05 22:41:30 +1000

    fix: right owl path

--- packages/icon-explorer/index.test.js
+++ packages/icon-explorer/index.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 
 // Setup the Owl client
-require('react-native-owl/dist/client').initClient();
+require('@johnf/react-native-owl/dist/client').initClient();
 
 // Load the app as normal
 require('./index');
