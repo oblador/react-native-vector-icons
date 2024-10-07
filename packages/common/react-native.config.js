4420a2a224522adcc72c247e3297090be4d2dd47
John Ferlito <johnf@inodes.org>
2024-10-07 16:28:47 +1100

    fix: s this it?

--- packages/common/react-native.config.js
+++ packages/common/react-native.config.js
@@ -5,9 +5,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        // NOTE: This is what create-react-native-library has but it doesn't seem to work
-        // cmakeListsPath: 'generated/jni/CMakeLists.txt',
-        cmakeListsPath: 'build/generated/source/codegen/jni/CMakeLists.txt',
+        cmakeListsPath: 'generated/jni/CMakeLists.txt',
       },
     },
   },
