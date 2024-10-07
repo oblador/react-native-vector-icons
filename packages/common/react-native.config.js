64cd5d75011c9869bfb87fb3df23cbeeac5a24b5
John Ferlito <johnf@inodes.org>
2024-10-07 13:27:39 +1100

    fix: align with latest create-react-native-library

--- packages/common/react-native.config.js
+++ packages/common/react-native.config.js
@@ -5,7 +5,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        cmakeListsPath: 'build/generated/source/codegen/jni/CMakeLists.txt',
+        cmakeListsPath: 'generated/jni/CMakeLists.txt',
       },
     },
   },
