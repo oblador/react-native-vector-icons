cbc6edfb9cfc34c7bd3b0b02a45244ec2bb5c299
John Ferlito <johnf@inodes.org>
2024-10-07 16:02:00 +1100

    fix: put cmake file path back

--- packages/common/react-native.config.js
+++ packages/common/react-native.config.js
@@ -5,7 +5,9 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        cmakeListsPath: 'generated/jni/CMakeLists.txt',
+        // NOTE: This is what create-react-native-library has but it doesn't seem to work
+        // cmakeListsPath: 'generated/jni/CMakeLists.txt',
+        cmakeListsPath: 'build/generated/source/codegen/jni/CMakeLists.txt',
       },
     },
   },
