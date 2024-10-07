4678b6bbaeb8444e76b53b7b623bf0c243d8b46e
John Ferlito <johnf@inodes.org>
2024-10-07 13:27:58 +1100

    fix: try test app

---
+++ packages/example/metro.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { getDefaultConfig } = require('@react-native/metro-config');
+const { getConfig } = require('react-native-builder-bob/metro-config');
+const pkg = require('../package.json');
+
+const root = path.resolve(__dirname, '..');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+module.exports = getConfig(getDefaultConfig(__dirname), {
+  root,
+  pkg,
+  project: __dirname,
+});
