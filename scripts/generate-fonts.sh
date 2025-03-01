e22151965621ebb46809b37698c7306c120c3def
John Ferlito <johnf@inodes.org>
2025-03-01 13:13:50 +1100

    fix: add diffenator so we can compare fonts

--- scripts/generate-fonts.sh
+++ scripts/generate-fonts.sh
@@ -19,19 +19,36 @@ for package in $PACKAGES; do
 
   cd $package
 
+  if [ -n "$DIFF" ]; then
+    mkdir -p "diffenator/$package"
+    mkdir -p "/tmp/$package/before"
+    cp fonts/*.ttf "/tmp/$package/before"
+  fi
+
   CURRENT_VERSION=$(jq -r '.version' package.json)
 
   rm -rf *
 
   if [ "$(jq -r '."generator-react-native-vector-icons".customReadme' .yo-rc.json)" == "true" ]; then
-    git restore README.md > /dev/null || true
+    git restore README.md >/dev/null || true
   fi
 
   if [ "$(jq -r '."generator-react-native-vector-icons".customSrc' .yo-rc.json)" == "true" ]; then
-    git restore src > /dev/null || true
+    git restore src >/dev/null || true
   fi
 
   yo react-native-vector-icons --force --current-version=$CURRENT_VERSION
+
+  if [ -n "$DIFF" ]; then
+    mkdir -p "/tmp/$package/after"
+    cp fonts/*.ttf "/tmp/$package/after"
+    for font in "/tmp/$package/before/"*.ttf; do
+      fontname=$(basename "$font")
+      mkdir -p "diffenator/$package/$fontname"
+      diffenator3 --no-tables --no-kerns --no-words --html --output "../../diffenator/$package/$fontname" "/tmp/$package/before/$fontname" "/tmp/$package/after/$fontname"
+      open "../../diffenator/$package/$fontname/diffenator.html"
+    done
+  fi
 
   cd -
 done
