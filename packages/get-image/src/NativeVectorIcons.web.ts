2aee571040f3ea19258169133b3c8192cda93255
Vojtech Novak <vonovak@gmail.com>
2025-07-11 11:29:12 +0200

    fix(web): throw a meaningful error in getImageForFont

--- packages/get-image/src/NativeVectorIcons.web.ts
+++ packages/get-image/src/NativeVectorIcons.web.ts
@@ -1,1 +1,12 @@
-export default {};
+import type { Spec } from './NativeVectorIcons';
+
+const webImpl: Spec = {
+  async getImageForFont() {
+    throw new Error('getImageForFont is not available for web');
+  },
+  getImageForFontSync() {
+    throw new Error('getImageForFontSync is not available for web');
+  },
+};
+
+export default webImpl;
