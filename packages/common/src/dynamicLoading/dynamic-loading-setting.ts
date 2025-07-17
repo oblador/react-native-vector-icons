6915f2f3f71258b506a76980a19d7aa0681edf3c
Vojtech Novak <vonovak@gmail.com>
2025-07-17 20:52:11 +0200

    feat: allow dynamic font loading with expo on web (#1807)

--- packages/common/src/dynamicLoading/dynamic-loading-setting.ts
+++ packages/common/src/dynamicLoading/dynamic-loading-setting.ts
@@ -1,3 +1,5 @@
+import { Platform } from 'react-native';
+
 import type { FontSource } from './types';
 
 type ExpoAssetModule = {
@@ -6,11 +8,14 @@ type ExpoAssetModule = {
   downloadAsync: (uri: string, hash: string | undefined, type: string) => Promise<string>;
 };
 
+// this is a file:// uri on native, or an object with uri and display on web
+export type LoadAsyncAsset = string | { uri: string; display: string };
+
 type ExpoFontLoaderModule = {
   // definition from
   // https://github.com/expo/expo/blob/1f5a5991d14aad09282d1ce1612b44d30e7e7d3d/packages/expo-font/ios/FontLoaderModule.swift#L18
   getLoadedFonts: () => string[];
-  loadAsync: (fontFamilyAlias: string, fileUri: string) => Promise<void>;
+  loadAsync: (fontFamilyAlias: string, asset: LoadAsyncAsset) => Promise<void>;
 };
 
 type ExpoFontUtilsModule = {
@@ -50,7 +55,7 @@ function getIsDynamicLoadingSupported(globalObj: any): globalObj is {
 } {
   return (
     globalObj?.expo &&
-    typeof globalObj.expo.modules?.ExpoAsset?.downloadAsync === 'function' &&
+    (Platform.OS === 'web' || typeof globalObj.expo.modules?.ExpoAsset?.downloadAsync === 'function') &&
     typeof globalObj.expo.modules?.ExpoFontLoader?.getLoadedFonts === 'function' &&
     typeof globalObj.expo.modules?.ExpoFontLoader?.loadAsync === 'function'
   );
@@ -69,11 +74,12 @@ export function getIsRenderToImageSupported(globalObj: any): globalObj is {
 
 export function assertExpoModulesPresent(globalObj: unknown): asserts globalObj is { expo: ExpoGlobalType } {
   if (!getIsDynamicLoadingSupported(globalObj)) {
-    throw new Error('Dynamic font loading for Expo is not available.');
+    throw new Error('Dynamic font loading not supported. Upgrade to latest expo and expo-font.');
   }
 }
 
-const hasNecessaryExpoModules = !!globalThis.expo?.modules?.ExpoAsset && !!globalThis.expo?.modules?.ExpoFontLoader;
+const hasNecessaryExpoModules =
+  (Platform.OS === 'web' || !!globalThis.expo?.modules?.ExpoAsset) && !!globalThis.expo?.modules?.ExpoFontLoader;
 
 const hasNecessaryExpoFeatures = getIsDynamicLoadingSupported(globalThis);
 
@@ -93,8 +99,8 @@ export const setDynamicLoadingEnabled = (value: boolean): boolean => {
   if (!hasNecessaryExpoFeatures) {
     if (process.env.NODE_ENV !== 'production' && !!value) {
       const message = hasNecessaryExpoModules
-        ? 'Expo is installed, but does not support dynamic font loading. Make sure to use Expo SDK 52 or newer.'
-        : 'Necessary Expo modules not found. Dynamic font loading is not available on Web or when necessary Expo modules are not present.';
+        ? 'Expo is installed, but does not support dynamic font loading. Make sure to use Expo SDK 54 or newer.'
+        : 'Necessary Expo modules not found. Dynamic font loading is not available when necessary Expo modules are not present.';
       console.error(message); // eslint-disable-line no-console
     }
     return false;
