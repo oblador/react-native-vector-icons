de11b287831fb086e494ac58c7f0b7967a86c903
Vojtech Novak <vonovak@gmail.com>
2025-07-20 18:18:08 +0200

    feat: add 'use client' directive for RSC (#1809)
    * feat: add 'use client' directive for RSC
    
    * feat: add 'use client' directive for RSC
    
    ---------
    
    Co-authored-by: John Ferlito <johnf@inodes.org>

--- packages/octicons/src/index.ts
+++ packages/octicons/src/index.ts
@@ -1,9 +1,9 @@
-// NOTE:This file was generated from packages/generator-react-native-vector-icons/src/app/templates
-// If you're contributing to react-native-vector-icons, make the change there, otherwise it'll be lost
+'use client';
 
 /**
  * This is a generated file. If you modify it manually, your changes will be lost!
- * Instead, modify the template in `generator-react-native-vector-icons`.
+ * This file was generated from packages/generator-react-native-vector-icons/src/app/templates
+ * If you're contributing to react-native-vector-icons, make the change there; otherwise it'll be lost
  *
  * Octicons icon set component.
  * Usage: <Octicons name="icon-name" size={20} color="#4F8EF7" />
