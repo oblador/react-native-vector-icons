fa946e6295178b614c3106f7231a4a6136628cad
John Ferlito <johnf@inodes.org>
2025-07-24 00:45:37 +1000

    feat: fix icon name type exports (#1817)

--- scripts/fix-glyphmaps.sh
+++ scripts/fix-glyphmaps.sh
@@ -14,3 +14,6 @@ $ISED 's/..\/glyphmaps/..\/..\/glyphmaps/' lib/commonjs/index.js
 $ISED 's/..\/fonts/..\/..\/fonts/' lib/commonjs/index.js
 $ISED 's/..\/glyphmaps/..\/..\/glyphmaps/' lib/module/index.js
 $ISED 's/..\/fonts/..\/..\/fonts/' lib/module/index.js
+
+$ISED 's/..\/glyphmaps/..\/..\/..\/..\/glyphmaps/' lib/typescript/commonjs/src/index.d.ts
+$ISED 's/..\/glyphmaps/..\/..\/..\/..\/glyphmaps/' lib/typescript/module/src/index.d.ts
