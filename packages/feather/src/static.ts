9fc8f4c147ab825e58df80714403a870b3d78346
Vojtech Novak <vonovak@gmail.com>
2026-03-20 11:02:29 +0100

    feat: expose static export for icon families (#1880)
    * feat: expose static export for icon families
    
    * docs: update readme
    
    * feat: static export for fontawesome icons
    
    * chore: do not check template sources with macros
    
    * chore: update `prepare` scripts to use `.mjs` version of `fix-glyphmaps`

---
+++ packages/feather/src/static.ts
@@ -0,0 +1,24 @@
+'use client';
+
+/**
+ * This is a generated file. If you modify it manually, your changes will be lost!
+ * This file was generated from packages/generator-react-native-vector-icons/src/app/templates
+ * If you're contributing to react-native-vector-icons, make the change there; otherwise it'll be lost
+ *
+ * Feather icon set component.
+ * Usage: <Feather name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import { createIconSet } from '@react-native-vector-icons/common';
+
+import glyphMap from '../glyphmaps/Feather.json';
+
+export const Feather = createIconSet(glyphMap, {
+  postScriptName: 'Feather',
+  fontFileName: 'Feather.ttf',
+});
+
+export type FeatherIconName = keyof typeof glyphMap;
+
+/** @alias */
+export default Feather;
---
+++ packages/generator-react-native-vector-icons/src/app/templates/src/static.ts
@@ -0,0 +1,24 @@
+'use client';
+
+/**
+ * This is a generated file. If you modify it manually, your changes will be lost!
+ * This file was generated from packages/generator-react-native-vector-icons/src/app/templates
+ * If you're contributing to react-native-vector-icons, make the change there; otherwise it'll be lost
+ *
+ * <%= className %> icon set component.
+ * Usage: <<%= className %> name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import { createIconSet } from '@react-native-vector-icons/<%= commonPackage %>';
+
+import glyphMap from '../glyphmaps/<%= className %>.json';
+
+export const <%= className %> = createIconSet(glyphMap, {
+  postScriptName: '<%= postScriptName %>',
+  fontFileName: '<%= fontFileName %>.ttf',
+});
+
+export type <%= className %>IconName = keyof typeof glyphMap;
+
+/** @alias */
+export default <%= className %>;
