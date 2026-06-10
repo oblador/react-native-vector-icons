15882ecfbf5fa3d36080f88ea3cc39056c528b9c
aydgnme <mertaydogn0@gmail.com>
2026-06-10 11:32:26 +0300

    feat: add Phosphor icon set (regular, 1530 icons)
    Adds @react-native-vector-icons/phosphor package based on
    Phosphor Icons v2.1.2 (https://phosphoricons.com).
    
    - 1530 icons from the regular style
    - Glyphmap parsed from official CSS
    - Expo config plugin for iOS font registration
    - Android native module (VectorIconsPhosphorPackage)
    - iOS podspec
    - Glyphmap integrity tests (7 tests)
    
    Closes #1677

---
+++ packages/phosphor/app.plugin.js
@@ -0,0 +1,8 @@
+const { withInfoPlist } = require('@expo/config-plugins');
+
+module.exports = (config) =>
+  withInfoPlist(config, (c) => {
+    const fonts = ['Phosphor.ttf'];
+    c.modResults.UIAppFonts = [...new Set([...(c.modResults.UIAppFonts || []), ...fonts])];
+    return c;
+  });
---
+++ packages/phosphor/src/index.ts
@@ -0,0 +1,21 @@
+'use client';
+
+/**
+ * Phosphor icon set component.
+ * Usage: <Phosphor name="airplane" size={20} color="#4F8EF7" />
+ */
+
+import { createIconSet } from '@react-native-vector-icons/common';
+
+import glyphMap from '../glyphmaps/Phosphor.json';
+
+export const Phosphor = createIconSet(glyphMap, {
+  postScriptName: 'Phosphor',
+  fontFileName: 'Phosphor.ttf',
+  fontSource: require('../fonts/Phosphor.ttf'),
+});
+
+export type PhosphorIconName = keyof typeof glyphMap;
+
+/** @alias */
+export default Phosphor;
